63cf3937cdc72ac517283abf07ae8ab56a94d00a
Altariel <agnese.sgarbi@gmail.com>
2014-02-09 01:39:59 +0100

    scritte tutte le funzioni fino a headprocq da testare

--- pcb.c
+++ pcb.c
@@ -5,31 +5,111 @@
 #include "asl.h"
 //head pointer to pcb_free list
 pcb_t *pcbFree_h;
+static pcb_t array[MAXPROC]; //array dei procblk
 
 /* List view functions */
 
 void freePcb(pcb_t *p){
 	pcb_t *app;
 	app=pcbFree_h;
+	//scorro fino a fine lista
 	while (app->p_next != NULL) {
 		app=app->p_next;
 	}
+	//aggiungo p in coda
 	app->p_next=p;
-	p->next=NULL;
+	p->p_next=NULL;
+	
 }
-pcb_t *allocPcb(void){
-	pcb_t app;
-};
-/*EXTERN pcb_t *allocPcb(void);
-EXTERN void initPcbs(void);
-*/
-//EXTERN pcb_t *mkEmptyProcQ(void);
-/*EXTERN int emptyProcQ(pcb_t *tp);
-EXTERN void insertProcQ(pcb_t **tp, pcb_t *p);
-EXTERN pcb_t *removeProcQ(pcb_t **tp);
-EXTERN pcb_t *outProcQ(pcb_t **tp, pcb_t *p);
-EXTERN pcb_t *headProcQ(pcb_t *tp);
-*/
+pcb_t *allocPcb (){
+	//se la lista non e' vuota allora restituisco in pcb con i valori inizializzati
+	if (pcbFree_h != NULL){
+		pcb_t *p;
+		p=pcbFree_h;
+		pcbFree_h->p_next = p->p_next;
+		p->p_next = NULL;
+		p->p_prnt = NULL;
+		p->p_child = NULL;
+		p->p_sib = NULL;
+		//p->p_s = ??;
+		//p->p_semAdd = ??;
+		return p;
+	} 
+	//se la lista e' vuota ritorno NULL
+	return NULL;
+}
+
+void initPcbs(void){
+	int i;
+	pcb_t *p;
+	for (i = 0; i < MAXPROC; i++){
+		if (i==0){
+			pcbFree_h = &array[i];
+			p=pcbFree_h;
+		}
+		if (i < MAXPROC-1){
+			p->p_next = &array[i];
+		}else{
+			p->p_next = NULL;
+		}
+		p=p->p_next;
+	}
+}
+
+pcb_t *mkEmptyProcQ(void){
+	pcb_t *tp;
+	tp = NULL;
+	return tp;
+}
+
+int emptyProcQ(pcb_t *tp){
+	if (tp == NULL){
+		return TRUE;
+	}
+	return FALSE;
+}
+
+void insertProcQ(pcb_t **tp, pcb_t *p){
+	p->p_next=(*tp)->p_next;
+	(*tp)->p_next=p;
+	*tp=p;
+}
+pcb_t *removeProcQ(pcb_t **tp){
+	pcb_t *p;
+	if (*tp == NULL){
+		return NULL;
+	}
+	p=(*tp)->p_next;
+	if (*tp == p){
+		*tp=NULL;
+		return p;
+	}
+	(*tp)->p_next = p->p_next;
+	return p;
+}
+pcb_t *outProcQ(pcb_t **tp, pcb_t *p){
+	if( *tp ==NULL){
+		return NULL;
+	}
+	
+	pcb_t *tmp;
+	tmp=p;
+	while (tmp->p_next!=p){
+		tmp=tmp->p_next;
+	}
+	tmp->p_next=p->p_next;
+	if( p == *tp){
+		*tp=tmp;
+	}
+	return p;
+}
+
+pcb_t *headProcQ(pcb_t *tp){
+	if (tp=NULL){
+		return NULL;
+	}
+	return tp->p_next;
+}
 /* Tree view functions */
 /*
 void initASL(void);
