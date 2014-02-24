f26a9bd1f4a5127a50fc7115b8ad471cc1502b8f
Baranzo <iz4ykn@gmail.com>
2014-02-24 03:05:28 +0100

    albero delle directory sistemato e makefile funzionante, consegnato in ritardo ovviamente :(

--- makefile
+++ makefile
@@ -1,38 +1,42 @@
-# This Makefile provide complete core building for UARM Emulator, from cross compiling to .elf to .uarm conversion.
-# The PATH of this file must be: /usr/include/uarm/test
+# This Makefile provide complete core building for UARM Emulator, from cross compiling to .elf to .uarm conversion
 # AUTHOR: Agnese Sgarbi 
 # DATE: 2014/02/06
 
 
 # Source file
-# Edit FILE for change the C source file you want to use for build the UARM core
-# WARNING: it work with only 1 source code file
+# Edit FILE* for change the C source file you want to use for build the UARM core
+
 
 FILE = p1test
-FILEPCB = pcb
-CFILE = $(FILE).c
+FILEPCB = src/pcb
+FILEASL = src/asl
+
+CFILE = src/$(FILE).c
 CFILEPCB = $(FILEPCB).c
-OFILE = $(FILE).o
+CFILEASL = $(FILEASL).c
+
+OFILE = src/$(FILE).o
 OFILEPCB = $(FILEPCB).o
+OFILEASL = $(FILEASL).o
+
 CORE = $(FILE).core.uarm
 
 # Compiling macros 
-# Complete command:  arm-none-eabi-gcc -mcpu=arm7tdmi -c -o p1test.o /usr/include/uarm/test/p1test.c
 
 CC = arm-none-eabi-gcc
 CO = -mcpu=arm7tdmi -c -o 
 
 
 # Linking macros
-# complete command:  arm-none-eabi-ld -T /usr/include/uarm/ldscripts/elf32ltsarm.h.uarmcore.x -o p1test /usr/include/uarm/crtso.o /usr/include/uarm/libuarm.o p1test.o
 
-LD = arm-none-eabi-ld -T ../ldscripts/elf32ltsarm.h.uarmcore.x -o
-LFILE = ../crtso.o ../libuarm.o $(OFILEPCB)
+LD = arm-none-eabi-ld -T ldscripts/elf32ltsarm.h.uarmcore.x -o
+LFILE = src/crtso.o src/libuarm.o $(OFILEPCB) $(OFILEASL) $(OFILE)
 
 
 # Conversion macros
-# complete command:  elf2uarm -k test/p1test.elf
+
 CONV = elf2uarm -k
+
 
 #Building the arm core 
 
@@ -41,22 +45,29 @@ $(CORE): $(FILE)
 	@echo "Converting to the UARM core file..."
 	$(CONV) $(FILE)
 
-
 #Linking the precompiled test file with the file necessary for build the .elf file
 $(FILE) : $(LFILE) $(OFILE)
 	@echo "Linkink necessary file with the obj file..."
-	$(LD) $(FILE) $(LFILE) $(OFILE)
+	$(LD) $(FILE) $(LFILE) 
+
+
+#CROSS COMPILING PHASE:
+
+#Cross compiling from the C source file asl.c into arm architeture
+$(OFILEASL) : $(CFILEASL)
+	@echo "Compiling asl into ARM architeture..." 
+	$(CC) $(CO) $(OFILEASL) $(CFILEASL)
 
 #Cross compiling from the C source file pcb.c into arm architeture
 $(OFILEPCB) : $(CFILEPCB)	
-	@echo "Compiling pcb into arm architeture"
+	@echo "Compiling pcb into ARM architeture"
 	$(CC) $(CO) $(OFILEPCB) $(CFILEPCB)
 
 #Cross compiling from the C source file into arm architeture
 $(OFILE) : $(CFILE)
-	@echo "Compiling into ARM architeture..." 
+	@echo "Compiling p1test into ARM architeture..." 
 	$(CC) $(CO) $(OFILE) $(CFILE)
 
 #Remove all created file
 cleanall :
-	rm  $(OFILE) $(FILE).stab.uarm $(FILE).core.uarm 
+	rm  $(OFILE) $(OFILEASL) $(OFILEPCB)  *.uarm $(FILE)
---
+++ src/asl.c
@@ -0,0 +1,140 @@
+
+#include "asl.h"
+
+semd_t *semdFree_h; // head pointer to semdFree list
+semd_t *semd_h; //active sema4 list head pointer
+static semd_t semdTable[MAXPROC + 1]; // semaphore descriptors array "+1" for the dummy block
+
+/* File with all the functions related to semaphore */ 
+
+/* Insert of the ProcBLk into the semaphore queue  */
+int insertBlocked ( int *semAdd, pcb_t *p ) {
+  semd_t *tmp = semd_h;
+
+  /* Find the correct element */
+  while( tmp->s_next != NULL && ( ( tmp->s_next )->s_semAdd ) < semAdd ){
+    tmp = tmp->s_next;
+  }
+  
+  /* Control if we must create a new semaphore */
+  if( tmp->s_next == NULL || ( tmp->s_next )->s_semAdd > semAdd ){
+    if( semdFree_h == NULL ){
+      return TRUE;
+    }
+    else{	
+      semd_t *add = semdFree_h;
+      semdFree_h = semdFree_h->s_next;
+      add->s_next = tmp->s_next;	
+      tmp->s_next = add;	
+      add->s_semAdd = semAdd;
+      add->s_procQ = mkEmptyProcQ();
+      add->s_procQ = p;
+      p->p_next = p;
+    }
+  }	
+  else {
+    if ( tmp->s_next->s_semAdd == semAdd ){
+      insertProcQ( &((tmp->s_next)->s_procQ ), p);	
+    }
+  }
+  p->p_semAdd = semAdd;
+  return FALSE;
+}
+
+
+/* Remove the head of the PCB queue of the given semaphore*/
+pcb_t *removeBlocked (int *semAdd){
+  semd_t *prec;
+  prec = semd_h;
+  
+  /* Find the correct element */
+  while(prec->s_next != NULL && (prec->s_next)->s_semAdd != semAdd){
+    prec=prec->s_next;
+  }
+
+  if(prec->s_next == NULL){
+    return NULL;
+  }
+  
+  /* Coltrol if we find the element and remove it if we did */
+  if((prec->s_next)->s_semAdd==semAdd){
+    pcb_t *ris , *tp;
+    semd_t *tmp;
+    tp=(prec->s_next)->s_procQ;
+    ris=removeProcQ(&tp);
+    if(!emptyProcQ(tp)){
+      return ris;
+    }
+    else{
+      tmp=prec->s_next;
+      prec->s_next=tmp->s_next;
+      tmp->s_next=semdFree_h;
+      semdFree_h=tmp;
+      return ris;
+    }
+  }
+  return NULL;
+}	
+
+
+/* Remove the procBlc pointed by p */
+pcb_t *outBlocked (pcb_t *p){
+  semd_t *tmp = semd_h->s_next;
+
+  /* Search for the semaphore descriptor equals to semaaphore value of p */
+  while ((tmp->s_semAdd) != (p->p_semAdd) && tmp != NULL){
+    tmp = tmp->s_next;
+  }
+  
+  /* Remove the pcb if we find it */
+  if (tmp != NULL){
+    pcb_t *tp;
+    tp = outProcQ(&(tmp->s_procQ), p);
+    return tp;	
+  }
+  
+  return NULL;
+}
+
+
+/* Return a pointer to the Head of the process queue with the same semaphore */
+pcb_t *headBlocked (int *semAdd){
+  semd_t *tmp = semd_h->s_next;
+
+  /* search for the semaphore descriptor with the same semaphore value */
+  while (tmp != NULL && (tmp->s_semAdd) != semAdd ){
+    tmp = tmp->s_next;
+  }
+
+  /* if we find the right semaphore descriptor and it is not empty, it has a head.  */
+  if (tmp != NULL && tmp->s_procQ != NULL){
+    return tmp->s_procQ->p_next;
+  }
+  else
+  // maybe there isn't a corresponding sema4 descriptor or maybe there is but it's empty
+  return NULL;
+}
+
+
+/* Initialization of the semdFree list */
+initASL(){
+  int i;
+  semd_t *p;
+  p = semdFree_h = &semdTable[1];
+  
+  /* DUMMY Initialization */
+  semd_h->s_semAdd = NULL;
+  semd_h->s_procQ = NULL;
+  semd_h->s_next = NULL;
+
+  /* adding semdTable blocks allocated in the semdFree list */
+  for (i = 0; i < MAXPROC + 1; i++){
+    if (i < MAXPROC){
+      p->s_next = &semdTable[i + 1];
+    }
+    else{
+      p->s_next = NULL;
+    }
+    p = p->s_next;
+  }
+}
