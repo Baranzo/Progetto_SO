333315a17f4f37cfe35a2dc0834165247fbcb758
Altariel <agnese.sgarbi@gmail.com>
2014-08-23 12:21:22 +0200

    set status register, initialized level 2 data structures, initialized all nucleus mantained variables

--- src/nucleusinizializaiont.c
+++ src/nucleusinizializaiont.c
@@ -7,6 +7,11 @@
 #include "libuarm.h"
 
 #define RAMTOP 0x2D4
+#define STATUS_CLEAR_M_I_F_BITS 0xFFFFFF20 
+
+//nucleus variables
+pcb_t *readyQueue, *currentProcess;
+int processCount,softBlockCount;
 
 void interruptHandler(){}
 void TLBHandler(){}
@@ -44,8 +49,25 @@ int main(void)
 	tmp[13*size]=(void*)RAMTOP;
 	
 	//status register inizialization
-	unsigned int status;
+	unsigned int status,value;
 	status=getSTATUS();
+	//metto a zero i irq fiq e status bits
+	status &= STATUS_CLEAR_M_I_F_BITS;
+	//ora li setto al valore desiderato
+	value=0xDF;
+	//fast interrupt e interrupt settati a 1 cosi sono disabilitati, credo
+	//i 5 bit meno significativi sono impostati a 0x1F  ovvero system/kernel mode
+	status|=value;
+	setSTATUS(status);
+	//inizializzazione delle strutture del livello 2
+	initPcbs();
+	initASL();
+	//inizialize nucleus mantained variables process count soft block count ready queue current process
+	processCount=0;
+	softBlockCount=0;
+	readyQueue=NULL;
+	currentProcess=NULL;
+	
 	
 	return 0;
 }
