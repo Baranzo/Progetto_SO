9516d82540c36ec1dc56357f60e35860c97d8932
Altariel <agnese.sgarbi@gmail.com>
2014-08-29 18:05:20 +0200

    sys1 terminata

--- src/syscall.c
+++ src/syscall.c
@@ -15,11 +15,21 @@ void create_process(state_t* proc_state)
 	state_t *parent_state;
 	parent=currentProcess;
 	parent_state=parent->p_s;
+	
 	new_proc=allocPcb();
 	if (new_proc==NULL){
+		//non sono riuscito ad allocare il pcb quindi l operazione viene abortita e metto in v0 del padre -1
 			parent_state[4*size]=(void *)(-1);
 			return;
 		}
+	
+	insertChild(parent,new_proc);
+	//copio lo stato del padre nel processo figlio e lo metto nella readyqueue
+	new_proc->p_s=parent->p_s;
+	insertProcQ(&readyQueue,new_proc);
+	//in caso di successo metto in v0 0
+	parent_state[4*size]=(void *)(0);
+	return;
 }
 	
 void syscallHandler(int sistype)
